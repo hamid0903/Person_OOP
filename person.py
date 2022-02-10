e84dd58afa96a8b41ec048390e1330aea6d2f3cb
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-02-10 17:19:06 +0500

    person04

--- person.py
+++ person.py
@@ -1,3 +1,9 @@
+from unicodedata import name
+
+
 class Person:
-    def __init__(self, first_name, last_name, bith_name):
+    def __init__(self, first_name, last_name, birth_date):
+        self.first_name=name
+        self.last_name=last_name
+        self.birth_date=birth_date
         
