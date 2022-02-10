8b6074b1be45ece7783af0dcee2d95a781e97dbb
Your Name <ubaydullayev@samtuit.uz>
2022-02-10 17:43:09 +0500

    with new methods2

--- person.py
+++ person.py
@@ -2,14 +2,18 @@ class Person:
     def __init__(self, first_name, last_name, birth_date) -> None:
         self.first_name = first_name
         self.last_name = last_name
-        self.berth_date = birth_date
-        
-    def Get_age(self, birth_date):
-        self.age = 2022 - birth_date
-        return self.age
+        self.birth_date = birth_date
+       
+    def Get_age(self):
+        age = 2022 - self.birth_date
+        return age
     
-    def get_full_name(self,first_name, last_name):
-        self.full_name = first_name + last_name
-        return self.full_name
+    
+    def get_full_name(self):
+        full_name = self.first_name + ' ' +self.last_name
+        return full_name
         
     
+x = Person("Malik", "Ubaydullaev", 1986)
+print(x.get_full_name(), x.Get_age())
+
