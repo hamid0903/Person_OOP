8fa216899d1f5ed261a87b5ea94de80c7ae80068
Your Name <ubaydullayev@samtuit.uz>
2022-02-10 17:30:58 +0500

    with new methods full name and age

--- person.py
+++ person.py
@@ -3,3 +3,13 @@ class Person:
         self.first_name = first_name
         self.last_name = last_name
         self.berth_date = birth_date
+        
+    def Get_age(self, birth_date):
+        self.age = 2022 - birth_date
+        return self.age
+    
+    def get_full_name(self,first_name, last_name):
+        self.full_name = first_name + last_name
+        return self.full_name
+        
+    
