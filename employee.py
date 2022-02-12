b7c9b911c694bac3963be83feb23421de6b7b4fd
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-02-12 22:20:35 +0900

    emp

--- employee.py
+++ employee.py
@@ -1,9 +1,11 @@
 from person import Person
 class Employee(Person):
-    def __init__(self,first_name,last_name,birth_date,major,):
+    def __init__(self,first_name,last_name,birth_date,title,salary):
         Person.__init__(self,first_name,last_name,birth_date)
-        self.first_name = first_name
+        self.title = title
+        self.salary=salary
 
-    def get_name(self):
-        return self.first_name
+
+    def get_salary(self):
+        return self.salary
     
